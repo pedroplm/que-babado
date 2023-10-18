e52dbc3bdec3002192438055a7bade5cb9b72a88
pedroplm <pedropalomo.ti@gmail.com>
2023-10-18 16:23:46 -0300

    adição de hyper links, novo carrossel de imagens

---
+++ scripts.js
@@ -0,0 +1,19 @@
+var counter = 1;
+//capitulo-1
+setInterval(function () {
+        document.getElementById("radio" + counter).checked = true;
+            counter++;
+            if (counter > 5) {
+                    counter = 1;
+            }  
+    }, 5000);
+
+//elas por elas 2
+var counter2 = 1
+setInterval(function () {
+        document.getElementById("segundoElasPorElas" + counter2).checked = true;
+            counter2++;
+            if (counter2 > 18) {
+                    counter2 = 1;
+            }  
+    }, 5000);
