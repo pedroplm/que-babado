f49733c6fd200a3d4b9e360012be90330ea68f4d
pedroplm <pedropalomo.ti@gmail.com>
2023-11-13 10:12:29 -0300

    correção de js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -26,14 +26,6 @@ function sleep(ms) {
 }
 
 var counter = 1;
-//capitulo-1
-setInterval(function () {
-    document.getElementById("radio" + counter).checked = true;
-    counter++;
-    if (counter > 5) {
-        counter = 1;
-    }
-}, 5000);
 
 /* //elas por elas
 let list = document.querySelector('.slider .list');
